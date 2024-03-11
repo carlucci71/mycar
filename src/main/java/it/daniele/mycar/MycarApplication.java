1f6e7ab84b3782e0ae3513d34fb842bcaca21a63
d.carlucci <carlucci.daniele@gmail.com>
2024-03-11 10:12:31 +0100

    exclude openai prop

--- src/main/java/it/daniele/mycar/MycarApplication.java
+++ src/main/java/it/daniele/mycar/MycarApplication.java
@@ -1,11 +1,16 @@
 package it.daniele.mycar;
 
+import cool.cena.openai.autoconfigure.OpenAiAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
-@ComponentScan(basePackages = {"it.daniele.mycar", "cool.cena.openai.autoconfigure"})
+@ComponentScan(basePackages = {"it.daniele.mycar", "cool.cena.openai.autoconfigure"},
+		excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+				classes = OpenAiAutoConfiguration.class))
+
 @SpringBootApplication
 public class MycarApplication {
 
